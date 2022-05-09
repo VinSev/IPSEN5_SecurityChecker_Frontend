dadf16c134c4fe21710a8f5a064beb80b2ad82a5
Roeland195 <47328692+Roeland195@users.noreply.github.com>
2022-05-09 15:02:01 +0200

    formvalidatie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {ScanService} from "../scan/scan.service";
 import {NgForm} from "@angular/forms";
 import { ValidatieService } from '../shared/validatie.service';
+import { homedir } from 'os';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,11 @@ import { ValidatieService } from '../shared/validatie.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
   constructor(private router: Router,
               private scanService: ScanService,
-              private validatieService: ValidatieService) {
-  }
+              private validatieService: ValidatieService) {}
 
-  public submit(name: HTMLInputElement, email: HTMLInputElement, website: HTMLInputElement, ownership: HTMLInputElement, form: NgForm): boolean {
+  public submit(name: HTMLInputElement, email: HTMLInputElement, website: HTMLInputElement, ownership: HTMLInputElement, form: NgForm): boolean {    
     for(let input of [name, email, website, ownership]) {
       if(!input.checkValidity()) {
         input.reportValidity();
@@ -33,29 +32,11 @@ export class HomeComponent {
     return true;
   }
 
-  //Preventing placing anything but alfabetic key types plus the minus (-) for names using -
-  keyPressAlphabet(event: Event){
-    //check type of event
-    const target = event.target as HTMLInputElement;
-    console.log(target.type);
-    
-    //create mask
-    switch((event.target as HTMLInputElement).type){
-      case "text":
-        //Name input
-        var mask = /[a-zA-ZáéóúíÁÉÓÚÍäëöüïÄËÖÜÏ-]/
-        break;
-      case "tell":
-        //Phone input
-        var mask = /[0-9+]/
-        break;
-      default:
-        //Email input
-        var mask = /[a-zA-Z0-9áéóúíÁÉÓÚÍäëöüïÄËÖÜÏ+_~@-]/
-        break;
-    }
-
-      this.validatieService.validateInputOfInputfield(event, mask);
-    
+  //Check if the given input is allowed in the given inputfield, if not let user know that this input is not allowed.
+  CheckInputValidation(event: Event){
+     if (!this.validatieService.validateInputOfInputfield(event)){
+     }else{
+       
+     }
   }
 }
--- src/app/shared/validatie.service.ts
+++ src/app/shared/validatie.service.ts
@@ -5,10 +5,26 @@ import { Injectable } from "@angular/core";
   })
   export class ValidatieService {
 
-    public  validateInputOfInputfield(event: any, mask: RegExp){
+    public validateInputOfInputfield(event: any){
+          //create mask
+    switch((event.target as HTMLInputElement).type){
+      case "text":
+        //Name input
+        var mask = /[a-zA-Z áéóúíÁÉÓÚÍäëöüïÄËÖÜÏ-]/
+        break;
+      case "tell":
+        //Phone input
+        var mask = /[0-9+ ]/
+        break;
+      default:
+        //Email input
+        var mask = /[a-zA-Z0-9 áéóúíÁÉÓÚÍäëöüïÄËÖÜÏ+_~@-]/
+        break;
+    }
+      
       var inp = String.fromCharCode(event.keyCode);
   
-      if(mask.test(inp)){
+      if(mask.test(inp) || event.keyCode == 8){
         return true;
       } else {
         event?.preventDefault();
