21d50de7de17e31c2c5778d50625dfee9f85611d
VinSe <vincentmcmaps@gmail.com>
2022-05-10 12:17:00 +0200

    Created and implemented funFactsService

--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,23 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ScanService} from "./scan.service";
 import {ToastrService} from "ngx-toastr";
 import {PdfService} from "../pdf/pdf.service";
+import {FunFactsService} from "./fun-facts.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.scss']
 })
-export class ScanComponent implements OnInit {
-  public funFacts: string[] = ["Hello", "Test", "World"];
+export class ScanComponent implements OnInit, OnDestroy {
+  private subscription!: Subscription;
+  public funFacts: string[] = [];
 
   constructor(public scanService: ScanService,
               private toastr: ToastrService,
-              private pdfService: PdfService) {
+              private pdfService: PdfService,
+              private funFactsService: FunFactsService) {
   }
 
   public ngOnInit() {
     this.startScan();
+    this.subscription = this.funFactsService.get()
+      .subscribe(response => {
+        this.funFacts = response;
+      });
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   private startScan(): void {
