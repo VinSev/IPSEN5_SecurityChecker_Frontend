c519c2dc326ff81a1761afe53061c841de6fa1d4
VinSe <vincentmcmaps@gmail.com>
2022-05-16 15:06:00 +0200

    scan tests

--- src/app/scan/scan/scan.component.spec.ts
+++ src/app/scan/scan/scan.component.spec.ts
@@ -22,4 +22,19 @@ describe('ScanComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return false if mail is empty', () => {
+    expect(component.submit(new HTMLInputElement())).toBeFalse();
+  });
+
+  it('should return true if mail is filled', () => {
+    let inputElement: HTMLInputElement = new HTMLInputElement();
+    inputElement.value = "hello@gmail.com";
+    expect(component.submit(inputElement)).toBeTrue();
+  });
+
+  it('should start scan', () => {
+    component.ngOnInit();
+    expect(component.scanService.scanCategories[0].loading).toBeTrue();
+  });
 });
