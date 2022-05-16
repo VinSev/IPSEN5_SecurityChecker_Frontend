4014f796bf0d73f4c70a221bd48232d2709ee160
VinSe <vincentmcmaps@gmail.com>
2022-05-16 15:06:07 +0200

    tips tests

---
+++ src/app/scan/tips/tips.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TipsComponent } from "./tips.component";
+
+describe('TipsComponent', () => {
+  let component: TipsComponent;
+  let fixture: ComponentFixture<TipsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TipsComponent ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TipsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should make connection', () => {
+    expect((<any>component).subscription).toBeDefined();
+  });
+});
