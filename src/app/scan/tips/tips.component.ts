c1d4aa8bd316a60491ce0bae79b4b059740e4e49
VinSe <vincentmcmaps@gmail.com>
2022-05-24 18:38:43 +0200

    implementing tips

--- src/app/scan/tips/tips.component.ts
+++ src/app/scan/tips/tips.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {TipsService} from "./tips.service";
+import {Tips} from "../../shared/models/tips.model";
 
 @Component({
   selector: 'app-tips',
@@ -9,12 +10,12 @@ import {TipsService} from "./tips.service";
 })
 export class TipsComponent implements OnInit, OnDestroy {
   private subscription!: Subscription;
-  public funFacts: string[] = [];
+  public funFacts: Tips[] = [];
 
   constructor(private tipsService: TipsService) { }
 
   public ngOnInit(): void {
-    this.subscription = this.tipsService.get()
+    this.subscription = this.tipsService.getAll()
       .subscribe(response => {
         this.funFacts = response;
       });
