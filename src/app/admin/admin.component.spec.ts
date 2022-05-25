25da4eb2f7d2303219a8a9e1ef36779fe9fd8b6c
VinSe <vincentmcmaps@gmail.com>
2022-05-25 10:37:27 +0200

    test fix

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {ToastrModule, ToastrService} from "ngx-toastr";
+import {FormsModule} from "@angular/forms";
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,7 +12,9 @@ describe('AdminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [ AdminComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+      providers: [{provide: ToastrService, useClass: ToastrModule}]
     })
     .compileComponents();
   });
--- src/app/shared/services/authentication.spec.ts
+++ src/app/shared/services/authentication.spec.ts
@@ -2,6 +2,7 @@ import {TestBed} from "@angular/core/testing";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
 import { authenticationService } from "./authentication.Service";
+import {ToastrModule, ToastrService} from "ngx-toastr";
 
 describe('HttpService', () => {
   let service: authenticationService;
@@ -9,6 +10,7 @@ describe('HttpService', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule ],
+      providers: [{provide: ToastrService, useClass: ToastrModule}]
     });
   });
 
