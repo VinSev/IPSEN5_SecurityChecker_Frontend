e6f0f68d86886e912129d8cd47520678670017b2
AntiEevee <eefjekarremans@hotmail.nl>
2022-06-21 22:39:35 +0200

    * Fix Router Tests * Fix ScanService Test * Fix missing Admin Routing Tests

--- src/app/admin/admin-home/admin-window/admin-scanresult/admin-scanresult.component.spec.ts
+++ src/app/admin/admin-home/admin-window/admin-scanresult/admin-scanresult.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminScanresultComponent } from './admin-scanresult.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {FormsModule} from "@angular/forms";
+import {ToastrModule, ToastrService} from "ngx-toastr";
+import {HttpService} from "../../../../shared/services/http.service";
 
 describe('AdminScanresultComponent', () => {
   let component: AdminScanresultComponent;
@@ -8,7 +13,9 @@ describe('AdminScanresultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminScanresultComponent ]
+      declarations: [ AdminScanresultComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+      providers: [{provide: ToastrService, useClass: ToastrModule}, HttpService]
     })
     .compileComponents();
   });
