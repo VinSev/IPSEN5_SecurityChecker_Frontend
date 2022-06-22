ae6e9dbd7051f582e3459ce3ab33b94751b4216a
VinSe <vincentmcmaps@gmail.com>
2022-06-22 06:40:26 +0200

    baseUrl fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: "https://web-security-scanner-frontend.herokuapp.com/",
+  baseURL: "https://web-security-scanner-backend.herokuapp.com/api/v1",
   captchaKey: "6LefCvUfAAAAAIRwp39gQlyPqW-OF3YkEyJVlo1f",
 };
